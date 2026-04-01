a1a60181c2e18d59cfad047e5567bbe596d1ff03
Kiran <smkiran.09@gmail.com>
2026-04-02 01:48:46 +0530

    deployment error fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./components/Home.jsx";
 import Layout from "./components/navigationFiles/Layout.jsx";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Layout />,
